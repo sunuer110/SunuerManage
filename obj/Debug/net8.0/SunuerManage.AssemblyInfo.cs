27b6f87c90b4afe6840fcf383e730b5824eacf87
sunuer <15161557+sunuer@user.noreply.gitee.com>
2025-03-08 13:28:46 +0800

    删除 obj 文件夹

--- obj/Debug/net8.0/SunuerManage.AssemblyInfo.cs
+++ obj/Debug/net8.0/SunuerManage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunuerManage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e24cf7347c384122544a92bf5f093c76467d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bb4e0e509d2375eb55f85351f17fb6fd22f62e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunuerManage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunuerManage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
